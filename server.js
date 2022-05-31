a2357971ad4955d2da7c9d661ca3d5f90370e53a
RodrigoFlexa <rodrigoflexa0211@gmail.com>
2022-05-31 18:09:02 -0300

    teste

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// importar express
 const express = require('express');
+// iniciar express
 const app = express();
+// nome da pasta no dist que sera feito o build
+const appName = 'qr-code-temp';
+// local onde build ira gerar os arquivos
+const outputPath = `${__dirname}/dist/${appName}`;
 
-const PORT = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/dist/qr-code-temp'));
-
-app.get('/*', (req,res) => {
-    res.sendFile(__dirname + '/dist/qr-code-temp/index.html');
-
+// seta o diretorio de build para servir o conteudo Angular
+app.use(express.static(outputPath));
+// redirecionar qualquer requisicao para o index.html
+app.get('/*', (req, res) => {
+  res.sendFile(`${outputPath}/index.html`);
 });
-
-app.listen(PORT, () => {
-    console.log('servidor iniciado na porta' + PORT);
-})
+// ouvir a porta que o Heroku disponibilizar
+app.listen(process.env.PORT);
